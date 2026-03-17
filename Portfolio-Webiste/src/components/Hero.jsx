6a2aa2c2f522fd90e77618079247fe0c4f631b51
Ayan Gupta <ayankrrish@gmail.com>
2026-03-17 22:29:14 +0530

    Changed hi to hey

--- Portfolio-Webiste/src/components/Hero.jsx
+++ Portfolio-Webiste/src/components/Hero.jsx
@@ -166,7 +166,7 @@ export default function Hero() {
             letterSpacing: "-2px",
           }}
         >
-          <span className="text-white">Hi, I'm</span>
+          <span className="text-white">Hey, I'm</span>
           <br />
           <span
             style={{
